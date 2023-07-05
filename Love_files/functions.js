148094bb23ce86cb090416cd47036115a252d47f
jun <sajiawuwu@users.noreply.github.com>
2023-07-06 01:56:07 +0800

    Update functions.js

--- Love_files/functions.js
+++ Love_files/functions.js
@@ -38,23 +38,14 @@ $(window).resize(function() {
 })(jQuery);
 
 function timeElapse(date){
-	var current = Date();
-	var seconds = (Date.parse(current) - Date.parse(date)) / 1000;
-	var days = Math.floor(seconds / (3600 * 24));
-	seconds = seconds % (3600 * 24);
-	var hours = Math.floor(seconds / 3600);
-	if (hours < 10) {
-		hours = "0" + hours;
-	}
-	seconds = seconds % 3600;
-	var minutes = Math.floor(seconds / 60);
-	if (minutes < 10) {
-		minutes = "0" + minutes;
-	}
-	seconds = seconds % 60;
-	if (seconds < 10) {
-		seconds = "0" + seconds;
-	}
-	var result = "第 <span class=\"digit\">" + days + "</span> 天 <span class=\"digit\">" + hours + "</span> 小时 <span class=\"digit\">" + minutes + "</span> 分钟 <span class=\"digit\">" + seconds + "</span> 秒"; 
+	var now = new Date(); // 获取当前日期和时间
+	var diff = Math.abs(now - date); // 计算日期差异的毫秒数
+
+	var diffDays = Math.floor(diff / (1000 * 3600 * 24)); // 计算天数差异
+	var diffHours = Math.floor((diff / (1000 * 3600)) % 24); // 计算小时数差异
+	var diffMinutes = Math.floor((diff / (1000 * 60)) % 60); // 计算分钟数差异
+	var diffSeconds = Math.floor((diff / 1000) % 60); // 计算秒数差异
+
+	var result = "第 <span class=\"digit\">" + diffDays + "</span> 天 <span class=\"digit\">" + diffHours + "</span> 小时 <span class=\"digit\">" + diffMinutes + "</span> 分钟 <span class=\"digit\">" + diffSeconds + "</span> 秒"; 
 	$("#clock").html(result);
 }
